55b44f59004a0b1adb070aebb47978b2a3412f9e
Angel Jiménez de Cisneros Carreño <angeljcc@gmail.com>
2015-05-05 19:33:40 +0200

    Creaaccion de la tabla 50x50 en JS

---
+++ Practica2/js/ejercicio02.js
@@ -0,0 +1,23 @@
+var color = "c3";
+var estaMarcado = false;
+var pincel = false;
+
+function PintarTablero(){
+
+    //creamos la tabla con nodos
+    var tabla = document.createElement("table");
+
+    //creamos la variable tablero que se asignará al nodo del elemento html
+    var tablero = document.getElementById("TableroDibujo");
+
+
+    //creamos la tabla como una matriz de 50x50
+
+    for(var i = 0; i < 50; i++){
+        var tr = document.createElement("tr");
+        for(var j = 0; j < 50)
+    }
+}
+
+
+
