119125277c180767cf1890cd0172851aa42a5c06
jarandaizquierdo <JaviAir@MacBook-Air-de-Javier.local>
2015-06-21 19:22:56 +0200

    Retocada la parte de visualizacion

--- Practica3_/visualizacion/index.php
+++ Practica3_/visualizacion/index.php
@@ -58,11 +58,11 @@
 
 
             <div class="row">
-                <div class="col-lg-4">
+                <div class="col-lg-3">
 
                 </div>
 
-                <div class="col-lg-4">
+                <div class="col-lg-6">
                     <div class="panel panel-default">
 
                         <div class="panel-body">
